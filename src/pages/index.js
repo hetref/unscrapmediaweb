7a6be0b0c179f0e8b5dfd4d3db8832b36be47752
Aryan shinde <shindearyan179@gmail.com>
2023-11-26 11:51:49 +0530

    META

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ScrollToTop from "../components/scroll-to-top";
-import SEO from "../components/seo";
+// import SEO from "../components/seo";
 import BrandContainer from "../containers/global/brand/index";
-import FunFactContainer from "../containers/global/funfact";
-import IconBoxContainer from "../containers/global/icon-box";
-import NewsletterArea from "../containers/global/newsletter";
+// import FunFactContainer from "../containers/global/funfact";
+// import IconBoxContainer from "../containers/global/icon-box";
+// import NewsletterArea from "../containers/global/newsletter";
 import TeamContainer from "../containers/global/team";
 import TestimonialContainer from "../containers/global/testimonial";
 import HomeAboutContainer from "../containers/home/about";
-import HomeBlog from "../containers/home/blog";
+// import HomeBlog from "../containers/home/blog";
 import IntroContainer from "../containers/home/intro";
 import ServiceListContainer from "../containers/service/service-list";
 import Footer from "../layouts/footer";
@@ -20,7 +20,7 @@ const HomePage = () => {
     return (
         <React.Fragment>
             <Layout>
-                <SEO title="Unscrap Media | Solution for Social Existence" />
+                {/* <SEO title="Unscrap Media | Solution for Social Existence" /> */}
                 <div className="wrapper">
                     <Header />
                     <IntroContainer />
